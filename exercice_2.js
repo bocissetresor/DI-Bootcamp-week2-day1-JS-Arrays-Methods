42efd21c6bcd1aa599e52be37a558603e2d24771
yanbert <yanbert@gmail.com>
2022-12-30 02:23:57 +0000

    voir mes commentaires dans le code

--- exercice_2.js
+++ exercice_2.js
@@ -1,1 +1,6 @@
+/*****************************************************
+Correcteur: Yannick GOUGUIA
+Exercice sans aucun commentaire ni sujet, il serait bien de mettre des commentaires et le contenu de la variable initiale
+*****************************************************/
+
 console.log(moreFruits[1][1]);
